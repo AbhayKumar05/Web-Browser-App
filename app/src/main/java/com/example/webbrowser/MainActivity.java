c60ac3db46c2a9bd810f69ada5f022ae126d72e2
Abhay Kumar <abhaylagah01@gmail.com>
2023-08-11 20:08:46 +0100

    key errors in AndroidManifest.xml file

---
+++ app/src/main/java/com/example/webbrowser/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.webbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+
+//web browser
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
