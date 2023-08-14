5d516b6539696aaf503ecbc939e3022c6ef046f7
Abhay Kumar <abhaylagah01@gmail.com>
2023-08-14 16:23:17 +0100

    key errors in AndroidManifest.xml file

--- app/src/main/java/com/example/webbrowser/MainActivity.java
+++ app/src/main/java/com/example/webbrowser/MainActivity.java
@@ -2,18 +2,68 @@ package com.example.webbrowser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
-
-
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 
 //web browser
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText urlInput;
+    ImageView clearUrl;
+    WebView webView;
+    ProgressBar progressBar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        urlInput = findViewById(R.id.url_input);
+        clearUrl = findViewById(R.id.clear_icon);
+        progressBar = findViewById(R.id.progress_bar);
+        webView = findViewById(R.id.web_view);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.getWebChromeClient(new WebChromeClient(){
+
+            public void
+        });
+
+    }
+}
+
+class MyWebViewClient extends WebViewClient{
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        return false;
+    }
+
+    @Override
+    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        super.onPageStarted(view, url, favicon);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+        progressBar.setVisibility(View.INVISIBLE);
     }
 }
